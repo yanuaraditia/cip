ae8e1021e3489cc9a0f2dc19c00356a271824aa7
Yanuar Aditia <yanuaraditia@outlook.com>
2019-10-10 15:07:36 +0700

    Fix maps action

--- application/views/content.php
+++ application/views/content.php
@@ -41,7 +41,7 @@ $this->load->helper('url');
                             if(isset($_COOKIE['latt'])&&isset($_COOKIE['long'])) {
                                 $km = getDistance($_COOKIE['latt'],$_COOKIE['long'],$data['lttd_lokasi'],$data['lgtd_lokasi'],'Km');
                                 $jarak = "<i class=\"material-icons\">near_me</i>".$km." km";
-                                echo anchor('https://maps.google.com/maps?q=-7.7900392,110.3654116',$jarak,'class="button is-light is-medium is-fullwidth"');
+                                echo anchor('https://maps.google.com/maps?q='.$data['lttd_lokasi'].','.$data['lgtd_lokasi'],$jarak,'class="button is-light is-medium is-fullwidth"');
                             }
                             else {
                                 $jarak = "<i class=\"material-icons\">near_me</i> Arah";
