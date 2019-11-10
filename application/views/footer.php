fbdbd1cb27530250d7b94320b41de9433a5b26ab
Yanuar Aditia <yanuaraditia@outlook.com>
2019-11-10 20:57:56 +0700

    Weloh

--- application/views/footer.php
+++ application/views/footer.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <p><b>Copyright &copy; 2020 by Sarana Teknotama</b></p>
         </div>
     </footer>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url().'/asset/jquery.min.js';?>"></script>
     <script>
     $(document).ready(function() {
     $(".navbar-burger").click(function() {
--- application/views/link_rel.php
+++ application/views/link_rel.php
@@ -29,7 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="apple-mobile-web-app-title" content="House of Yanuar Aditia">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <link rel="shortcut icon" href="https://paparkir.com/favico.png" type="image/png" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <link rel="stylesheet" href="<?php echo base_url().'/asset/bulma.min.css';?>" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500|Titillium+Web:400,600,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo base_url().'/asset/style.css';?>" />
 </head>
